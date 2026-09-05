065f4be3c54029f84755f0440dd9acf2d8fd166e
Matuwamai <wamaimatu@gmail.com>
2026-09-05 10:37:30 +0300

    Create super admin logics

---
+++ controllers/superAdmin.js
@@ -0,0 +1,14 @@
+import asyncHandler from '../utils/asyncHandler.js';
+import superAdminService from '../services/superAdmin.service.js';
+ 
+const login = asyncHandler(async (req, res) => {
+  const result = await superAdminService.login(req.body);
+  res.status(200).json(result);
+});
+ 
+const create = asyncHandler(async (req, res) => {
+  const admin = await superAdminService.createSuperAdmin(req.body);
+  res.status(201).json(admin);
+});
+ 
+export default { login, create };
